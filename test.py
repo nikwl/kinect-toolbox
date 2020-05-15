4fb47bb9d3ef0650b97f5807f687f84cef415047
nikolas <nikolas.lamb@gmail.com>
2020-05-15 15:14:22 -0400

    changes to package structure

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ k = Kinect()
 
 while True:
     # Specify as many types as you need here
-    color_frame = k.get_frame([KinectIm.COLOR])[0]
+    color_frame = k.get_frame([KinectIm.RAW_COLOR])[0]
 
     cv2.imshow('frame', color_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
