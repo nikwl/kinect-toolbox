a0941244a4f926d3fc0617e7d8efd2d2e9514c06
nikolas <nikolas.lamb@gmail.com>
2020-08-23 14:57:35 -0400

    fixed test case, previous commit broke

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import cv2
 from kinecttb import Kinect, KFrame
 
-k = Kinect(headless=True)
+k = Kinect()
 while True:
     # Specify as many types as you want here
     color_frame = k.get_frame(KFrame.COLOR)
 
-    break
     cv2.imshow('frame', color_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
